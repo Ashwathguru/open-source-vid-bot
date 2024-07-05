535e30566b75500d3bba539db3e9a04a24088cfb
Ashwath <ashwathguru.s@gmail.com>
2024-07-05 11:14:00 +0530

    changes

--- integrator.py
+++
@@ -1,7 +0,0 @@
-import bot2 as cb
-conversation_context = ""
-question = "What is naturopathy?"
-response = cb.get_response_from_bot(question)
-conversation_context += f"Question: {question}\nAnswer: {response}\n"
-cb.store_context(conversation_context)
-print(response)
